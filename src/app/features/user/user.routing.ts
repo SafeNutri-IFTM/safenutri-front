76edc62121d769b2718b06c9c4981c4d61d4b920
Desenvolvedor <dev@safenutri.com.br>
2026-04-04 16:23:04 -0300

    SN-65: criação da tela de cadastro de restrições alimentares

--- src/app/features/user/user.routing.ts
+++ src/app/features/user/user.routing.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 
 import { CadastroUserComponent } from './cadastro-user/cadastro-user.component';
+import { CadastroRestricaoComponent } from './cadastro-restricao/cadastro-restricao.component';
 
 export const UserRoutes: Routes = [
     {
         path: 'register',
         component: CadastroUserComponent
+    },
+    {
+        path: 'restricoes',
+        component: CadastroRestricaoComponent
     }
 ];
